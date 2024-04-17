a9f3ce77030a129c1e17f3200d37b3147dee521d
FlixMor <flixmo21@gmail.com>
2024-04-16 21:58:25 -0400

    felix16/04:21.58

--- scripttestFM.py
+++ scripttestFM.py
@@ -5,10 +5,16 @@ from Users.UserDAO import UserDAO
 from Users.User import User
 from Events.EventDAO import EventDAO
 from Events.Event import Event
-import database
+from Paiements.Paiement import Paiement
+from Paiements.PaiementDAO import PaiementDAO
 
 message, user = UserDAO.get_user_by_username('allo')
 message2, event = EventDAO.get_event_by_name('Evenement')
-message3 = ReservationDAO.add(user,event,15)
-reservations = ReservationDAO.get_reservation_by_user(user)
-#ReservationDAO.del_reservation(user,event)
+#carte = Paiement('allo','454548644896','45868')
+#message3 = ReservationDAO.add(user,event,15)
+#reservations = ReservationDAO.get_reservation_by_user(user)
+ReservationDAO.del_reservation(user,event)
+#print(event)
+#print(user)
+#PaiementDAO.add(user,carte)
+#PaiementDAO.pay(user,event)
